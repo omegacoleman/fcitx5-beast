1d996cf7ac4d7ae83296835ff80c48e9dd0bd578
youcai <omegacoleman@gmail.com>
2024-06-01 21:59:01 +0800

    add more tests to cover all config shifts

--- test/testbeast.cpp
+++ test/testbeast.cpp
@@ -27,12 +27,6 @@ int main() {
     auto beast = dynamic_cast<Beast *>(instance.addonManager().addon("beast"));
     assert(beast);
 
-    RawConfig default_port_raw;
-    BeastConfig default_port_cfg;
-    default_port_cfg.communication.setValue(BeastCommunication::Tcp);
-    default_port_cfg.save(default_port_raw);
-    beast->setConfig(default_port_raw);
-
     bool getterCalled = false;
     bool setterCalled = false;
 
@@ -45,28 +39,40 @@ int main() {
         return "";
     });
 
-    // Listen on default port.
-    assert(std::system("lsof -i:32489") == 0);
+    // Listen on default unix socket.
+    assert(std::system("lsof /tmp/fcitx5.sock") == 0);
 
     // Getter works.
     assert(!getterCalled);
-    assert(std::system("curl http://localhost:32489/config/addon/beast") == 0);
+    assert(std::system("curl --unix-socket /tmp/fcitx5.sock "
+                       "http://fcitx/config/addon/beast") == 0);
     assert(getterCalled);
 
     // Setter works.
     assert(!setterCalled);
-    assert(
-        std::system(
-            "curl -X POST -d '{}' http://localhost:32489/config/addon/beast") ==
-        0);
+    assert(std::system("curl --unix-socket /tmp/fcitx5.sock -X POST -d '{}' "
+                       "http://fcitx/config/addon/beast") == 0);
     assert(setterCalled);
 
-    // Port reset works.
+    // Unix socket path reset works.
     auto config = dynamic_cast<const BeastConfig *>(beast->getConfig());
     RawConfig raw;
     config->dumpDescription(raw);
     BeastConfig cfg;
+    cfg.unix_socket.mutableValue()->path.setValue("/tmp/fcitx5-another.sock");
+    cfg.save(raw);
+    beast->setConfig(raw);
+
+    assert(std::system("lsof /tmp/fcitx5.sock") != 0);
+    assert(std::system("lsof /tmp/fcitx5-another.sock") == 0);
+
+    // Tcp default port works.
     cfg.communication.setValue(BeastCommunication::Tcp);
+    cfg.save(raw);
+    beast->setConfig(raw);
+    assert(std::system("lsof -i:32489") == 0);
+
+    // Tcp port reset works.
     cfg.tcp.mutableValue()->port.setValue(32490);
     cfg.save(raw);
     beast->setConfig(raw);
